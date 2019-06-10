2e424ccbf5334907c2a1f7f2da6b75c9a696bf91
fotcorn <fotcorn@gmail.com>
2019-06-10 02:16:37 +0200

    reformat code

--- instruction_set.py
+++ instruction_set.py
@@ -1,5 +1,6 @@
 from enum import Enum, unique
 
+
 @unique
 class InstructionType(Enum):
     LD = 0x0001
@@ -33,13 +34,13 @@ class InstructionType(Enum):
     JE = 0x0402
     JNE = 0x0403
 
-    ## signed
+    # signed
     JL = 0x0501
     JLE = 0x0502
     JG = 0x0503
     JGE = 0x0504
 
-    ## unsigned
+    # unsigned
     JLU = 0x0601
     JLEU = 0x0602
     JGU = 0x0603
@@ -68,15 +69,15 @@ class ParameterType(Enum):
     IMMEDIATE_TWO_BYTE = 0x4
     IMMEDIATE_FOUR_BYTE = 0x5
     IMMEDIATE_EIGHT_BYTE = 0x6
-    
+
 
 @unique
 class Register(Enum):
-    i0 = 0x01
-    i1 = 0x02
-    i2 = 0x03
-    i3 = 0x04
-    i4 = 0x05
-    i5 = 0x06
-    i6 = 0x07
-    i7 = 0x08
+    I0 = 0x01
+    I1 = 0x02
+    I2 = 0x03
+    I3 = 0x04
+    I4 = 0x05
+    I5 = 0x06
+    I6 = 0x07
+    I7 = 0x08
--- interpreter.py
+++ interpreter.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 
+
 @dataclass
 class MachineState:
     i0: int = 0
