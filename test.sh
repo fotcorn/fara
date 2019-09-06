c2695e08cf1f8eb9b54ff0fe0e46ef87c8b90cff
fotcorn <fotcorn@gmail.com>
2019-09-06 09:46:40 +0200

    script to run automatic tests

---
+++ test.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+venv/bin/python compiler.py examples/hello_world.asm
+venv/bin/python compiler.py examples/add.asm
+venv/bin/python compiler.py examples/loop.asm
+venv/bin/python compiler.py examples/main.asm
+
+venv/bin/python emulator.py examples/hello_world.bin
+venv/bin/python emulator.py examples/add.bin
+# venv/bin/python emulator.py examples/loop.bin
+# venv/bin/python emulator.py examples/main.bin
