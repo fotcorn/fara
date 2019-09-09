c05290b3771fde454e6247e4aa0cd9e888b1196b
fotcorn <fotcorn@gmail.com>
2019-09-09 13:48:39 +0200

    remove main example

--- test.sh
+++ test.sh
@@ -6,11 +6,9 @@ venv/bin/python compiler.py examples/add.asm
 venv/bin/python compiler.py examples/size.asm
 venv/bin/python compiler.py examples/loop.asm
 venv/bin/python compiler.py examples/jump.asm
-venv/bin/python compiler.py examples/main.asm
 
 venv/bin/python emulator.py examples/hello_world.bin
 venv/bin/python emulator.py examples/add.bin
 venv/bin/python emulator.py examples/size.bin
 venv/bin/python emulator.py examples/loop.bin
 venv/bin/python emulator.py examples/jump.bin
-# venv/bin/python emulator.py examples/main.bin
