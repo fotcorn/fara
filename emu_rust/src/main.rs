6af07b9d256514ee768d74a1aabcbbedc1260ad1
fotcorn <fotcorn@gmail.com>
2019-12-12 15:00:56 +0000

    remu: load input file into memory

--- emu_rust/src/main.rs
+++ emu_rust/src/main.rs
@@ -1,6 +1,23 @@
+extern crate clap;
+use clap::{App, Arg};
+
+use std::fs::File;
+use std::io::Read;
+
 mod instruction_set;
 mod machine_state;
 
 fn main() {
-    let ms = machine_state::MachineState::new();
+    let matches = App::new("emulator")
+        .arg(Arg::with_name("file").required(true))
+        .get_matches();
+    let filename = matches.value_of("file").unwrap();
+
+    let mut file = File::open(filename).expect("Cannot open file");
+    let mut buffer = Vec::new();
+    file.read_to_end(&mut buffer).expect("Failed to read file.");
+
+    let mut ms = machine_state::MachineState::new();
+
+    ms.memory[0x1000..0x1000 + buffer.len()].copy_from_slice(&buffer);
 }
