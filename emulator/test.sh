b5b77b11baf4f867bca7373e7cd6ca47c4386048
fotcorn <fotcorn@gmail.com>
2023-01-02 18:41:18 +0000

    run execution tests generated by clang instead of python assembler

--- emulator/test.sh
+++ emulator/test.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -euxo pipefail
-cargo run ../examples/add.bin
-cargo run ../examples/hello_world.bin
-cargo run ../examples/jump.bin
-cargo run ../examples/loop.bin
-cargo run ../examples/size.bin
-cargo run ../examples/stack.bin
-cargo run ../examples/subroutine.bin
+cargo run -- --loader elf ../examples/add.o
+cargo run -- --loader elf ../examples/hello_world.o
+cargo run -- --loader elf ../examples/jump.o
+cargo run -- --loader elf ../examples/loop.o
+cargo run -- --loader elf ../examples/size.o
+cargo run -- --loader elf ../examples/stack.o
+cargo run -- --loader elf ../examples/subroutine.o
