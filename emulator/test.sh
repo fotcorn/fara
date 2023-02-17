731f50394a6c50636a4154c3e1f5ff8997a66c2f
fotcorn <fotcorn@gmail.com>
2023-02-17 18:14:05 +0000

    remove old build file

--- emulator/test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euxo pipefail
-cargo run -- --loader elf ../examples/add.o
-cargo run -- --loader elf ../examples/hello_world.o
-cargo run -- --loader elf ../examples/jump.o
-cargo run -- --loader elf ../examples/loop.o
-cargo run -- --loader elf ../examples/size.o
-cargo run -- --loader elf ../examples/stack.o
-cargo run -- --loader elf ../examples/subroutine.o
